a82b77217296164283660ef39f70d4f6c2c366ed
Eugene Petrenko <eugene.petrenko@gmail.com>
2013-03-28 00:30:36 +0100

    use only one state machine

--- src/mooo/PatternResolver.java
+++ src/mooo/PatternResolver.java
@@ -18,25 +18,19 @@ public class PatternResolver {
 
   @NotNull
   public Set<File> resolveWildcards(@NotNull File root, @NotNull Collection<String> includes) {
-    final List<StateMachine> state = new ArrayList<StateMachine>();
+    final List<Wildcard> state = new ArrayList<Wildcard>();
     for (String include : includes) {
-      state.add(new StateMachine(myFactory.parseWildcard(include)));
+      state.addAll(myFactory.parseWildcard(include));
     }
 
     final Set<File> result = new LinkedHashSet<File>();
-    visitFS(root, result, state);
+    visitFS(root, result, new StateMachine(state));
     return result;
   }
 
-  private boolean visitFollows(@NotNull File root, @NotNull Set<File> result, @NotNull Collection<StateMachine> state) {
-    if (state.isEmpty()) return true;
-
-    final Collection<String> follows = new ArrayList<String>();
-    for (StateMachine m : state) {
-      Collection<String> fs = m.follows();
-      if (fs == null) return false;
-      follows.addAll(fs);
-    }
+  private boolean visitFollows(@NotNull File root, @NotNull Set<File> result, @NotNull StateMachine state) {
+    final Collection<String> follows = state.follows();
+    if (follows == null) return false;
 
     for (String follow : follows) {
       final File path = new File(root, follow);
@@ -46,9 +40,7 @@ public class PatternResolver {
     return true;
   }
 
-  private void visitFS(@NotNull File root, @NotNull Set<File> result, @NotNull Collection<StateMachine> state) {
-    if (state.isEmpty()) return;
-
+  private void visitFS(@NotNull File root, @NotNull Set<File> result, @NotNull StateMachine state) {
     if (visitFollows(root, result, state)) return;
 
     final File[] allFiles = root.listFiles();
@@ -59,33 +51,16 @@ public class PatternResolver {
     }
   }
 
-  private void visitFileSystemEntry(@NotNull File path, @NotNull Set<File> result, @NotNull Collection<StateMachine> state) {
-    if (path.isFile()) {
-      visitFile(path, result, state);
-    } else {
-      visitDirectory(path, result, state);
-    }
-  }
-
-  private void visitFile(@NotNull File path, @NotNull Set<File> result, @NotNull Collection<StateMachine> state) {
-    for (StateMachine m : state) {
-      if (m.matchFiles(path.getName())) {
-        result.add(path);
-        break;
-      }
-    }
-  }
-
-  private void visitDirectory(@NotNull File path, @NotNull Set<File> result, @NotNull Collection<StateMachine> state) {
+  private void visitFileSystemEntry(@NotNull File path, @NotNull Set<File> result, @NotNull StateMachine state) {
     final String name = path.getName();
-    final List<StateMachine> nextState = new ArrayList<StateMachine>(state.size());
-    for (StateMachine m : state) {
-      StateMachine next = m.advance(name);
-      if (next != null) {
-        nextState.add(next);
-      }
-    }
 
-    visitFS(path, result, nextState);
+    if (path.isDirectory()) {
+      final StateMachine next = state.advance(name);
+      if (next != null) {
+        visitFS(path, result, next);
+      }
+    } else if (state.matchFiles(name)) {
+      result.add(path);
+    }
   }
 }
